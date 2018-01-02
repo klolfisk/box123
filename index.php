e8e81a2a8516a8d7017cbf820c3bbc7a218096d8
klolfisk <oskar.furberg@gmail.com>
2018-01-02 13:44:56 +0100

    fixed indentation

--- index.php
+++ index.php
@@ -41,25 +41,25 @@
 
 <!-- script for slideshow -->
 <script>
-  var slideIndex = 0;
-  slideshow();
+ 	var slideIndex = 0;
+	slideshow();
 
-  function slideshow() {
-      var i;
-      var slides = document.getElementsByClassName("images");
-      var dots = document.getElementsByClassName("dot");
-      for (i = 0; i < slides.length; i++) {
-         slides[i].style.display = "none";
-      }
-      slideIndex++;
-      if (slideIndex> slides.length) {slideIndex = 1}
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";
-      dots[slideIndex-1].className += " active";
-      setTimeout(slideshow, 2000); // Change image every 2 seconds
-  }
+	function slideshow() {
+		var i;
+  		var slides = document.getElementsByClassName("images");
+      	var dots = document.getElementsByClassName("dot");
+      	for (i = 0; i < slides.length; i++) {
+        	slides[i].style.display = "none";
+      	}
+      	slideIndex++;
+      	if (slideIndex> slides.length) {slideIndex = 1}
+      	for (i = 0; i < dots.length; i++) {
+          	dots[i].className = dots[i].className.replace(" active", "");
+      	}
+      	slides[slideIndex-1].style.display = "block";
+      	dots[slideIndex-1].className += " active";
+      	setTimeout(slideshow, 2000); // Change image every 2 seconds
+  	}
 </script>
 </body>
 </html>
